04dedb43db1b21b33f876abf66dafb42ef9c2406
a-lvaro <aaflneto@gmail.com>
2022-10-17 19:01:43 -0300

    melhorias

--- emailBadge/main.py
+++ emailBadge/main.py
@@ -3,10 +3,11 @@ from readData import ReadData
 
 
 df = ReadData().link(
-    'https://docs.google.com/spreadsheets/d/1MEqYORuZvMLv7qObalqSclSWGjnGDNTkbrCwsjr6SmQ/edit?usp=sharing',
+    'https://docs.google.com/spreadsheets/d/19v67ITpdtL9e0LudKP0kSfskPVrTYRkQIGhqDbVdYEo/edit?usp=sharing',
     save_subscribers=False)
 
+print(df)
 
-for i in range(df.shape[0]):
-    sendEmail(i, df['email'][i], df['nome'][i], df['cpf'][i],
-              badge=False)
+# for i in range(df.shape[0]):
+#     sendEmail(i, df['email'][i], df['nome'][i], df['cpf'][i],
+#               badge=False)
