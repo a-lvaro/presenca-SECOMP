694858a9ebfccc758f14a0a6df9d3ea556bbf278
a-lvaro <aaflneto@gmail.com>
2022-10-17 19:03:14 -0300

    up

--- emailBadge/main.py
+++ emailBadge/main.py
@@ -3,11 +3,11 @@ from readData import ReadData
 
 
 df = ReadData().link(
-    'https://docs.google.com/spreadsheets/d/19v67ITpdtL9e0LudKP0kSfskPVrTYRkQIGhqDbVdYEo/edit?usp=sharing',
+    'link',
     save_subscribers=False)
 
 print(df)
 
-# for i in range(df.shape[0]):
-#     sendEmail(i, df['email'][i], df['nome'][i], df['cpf'][i],
-#               badge=False)
+for i in range(df.shape[0]):
+    sendEmail(i, df['email'][i], df['nome'][i], df['cpf'][i],
+              badge=False)
