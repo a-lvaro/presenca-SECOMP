32ebdd66446faa5c01747c67608b1c6776bbd6e7
a-lvaro <aaflneto@gmail.com>
2022-12-17 14:06:09 -0300

    melhorias

--- lerQrcode/lerQrCode.py
+++ lerQrcode/lerQrCode.py
@@ -1,7 +1,5 @@
 import cv2
 import numpy as np
-import pandas as pd
-import os
 from pyzbar.pyzbar import decode
 from datetime import datetime
 from somMario import SomMario
@@ -9,65 +7,38 @@ from presenca import Presenca
 
 
 class LerQrCode():
-
-    def __setFile(self, path: str) -> None:
-        with open(path, 'w') as f:
-            f.write('data,hora,cpf,nome\n')
-
-    def __getFile(self, path: str):
-        data = datetime.today().strftime('%Y-%m-%d')
-        path = f'{path}/presenca.csv'
-        fields = {'data': str, 'hora': str, 'cpf': str, 'nome': str}
-
-        if not os.path.exists(path):
-            self.__setFile(path)
-
-        df = pd.read_csv(path, usecols=fields, dtype=fields)
-        df['cpf'] = df['cpf'].astype(str)
-
-        return df[df['data'] == data][['hora', 'nome', 'cpf']]
-
-    def __checarInscricao(self, path: str):
-        inscritos = {}
-
-        with open(f'{path}/inscritos.txt') as f:
-            myDataList = f.read().splitlines()
-
-        for data in myDataList:
-            key, value = data.split('|')
-            inscritos[key] = value
-
-        return inscritos
-
-    def __saveData(self, path, cpf, nome):
-
-        data = datetime.today().strftime('%Y-%m-%d')
-        hora = datetime.today().strftime('%H-%M-%S')
-        fields = {'data': str, 'hora': str, 'cpf': str, 'nome': str}
-
-        with open(f'{path}/presenca.csv', 'a') as p:
-            p.write(data + ',' + hora + ',' + cpf + ',' + nome + '\n')
-
-        df = pd.read_csv(f'{path}/presenca.csv', usecols=fields, dtype=fields)
-
-        return df[df['data'] == data][['hora', 'nome', 'cpf']]
-
-    def camera(self, nome_evento: str):
-        mario = SomMario()
-        presenca = Presenca()
-
+    def __init__(self, nome_evento: str):
         path = f'InscritosPresentes/{nome_evento}'
 
-        df_presenca = self.__getFile(path)
-        # pegar lista de inscritos no evento
-        inscritos = self.__checarInscricao(path)
+        self.__mario = SomMario()
+        self.__presenca = Presenca(path)
 
+    def __inicializarCamera(self):
         cap = cv2.VideoCapture(0)
         cap.set(3, 940)
         cap.set(4, 780)
 
-        while True:
+        return cap
 
+    def __molduraQrCode(self, img, barcode, qrcodeColor: tuple, nome: str) -> None:
+        pts = np.array([barcode.polygon], np.int32)
+        pts = pts.reshape((-1, 1, 2))
+        cv2.polylines(img, [pts], True, qrcodeColor, 5)
+        pts2 = barcode.rect
+
+        cv2.putText(img, nome, (pts2[0], pts2[1]), cv2.FONT_HERSHEY_SIMPLEX,
+                    0.9, qrcodeColor, 2)
+
+    def camera(self) -> None:
+        df_presenca = self.__presenca.getFile()
+        inscritos = self.__presenca.checarInscricao()
+
+        verde = (0, 255, 0)
+        vermelho = (0, 0, 255)
+
+        cap = self.__inicializarCamera()
+
+        while True:
             _, img = cap.read()
 
             for barcode in decode(img):
@@ -75,42 +46,33 @@ class LerQrCode():
                     'utf-8').encode('shift-jis').decode('utf-8')
 
                 if qrCodeCPF in inscritos.keys():        # match CPF
-                    qrcodeColor = (0, 255, 0)
-                    mario.tocar('inscrito')
+                    qrcodeColor = verde
+                    nome = inscritos[qrCodeCPF]
+                    self.__mario.tocar('inscrito')
 
                     if qrCodeCPF not in df_presenca['cpf'].tolist():
-                        df_presenca = self.__saveData(path, qrCodeCPF,
-                                                      inscritos[qrCodeCPF])
+                        df_presenca = self.__presenca.salvarDados(
+                            qrCodeCPF, inscritos[qrCodeCPF])
 
                     else:
                         horas = df_presenca[df_presenca['cpf']
                                             == qrCodeCPF]['hora'].tolist()
 
                         if max(horas)[:2] != datetime.today().strftime('%H'):
-                            df_presenca = self.__saveData(
-                                path, qrCodeCPF, inscritos[qrCodeCPF])
+                            df_presenca = self.__presenca.salvarDados(
+                                qrCodeCPF, inscritos[qrCodeCPF])
 
                 else:
-                    qrcodeColor = (0, 0, 255)
-                    mario.tocar('naoInscrito')
-
-                pts = np.array([barcode.polygon], np.int32)
-                pts = pts.reshape((-1, 1, 2))
-                cv2.polylines(img, [pts], True, qrcodeColor, 5)
-                pts2 = barcode.rect
-
-                if qrcodeColor == (0, 0, 255):
+                    qrcodeColor = vermelho
                     nome = 'nao inscrito no workshop'
-                else:
-                    nome = inscritos[qrCodeCPF]
+                    self.__mario.tocar('naoInscrito')
 
-                cv2.putText(img, nome, (pts2[0], pts2[1]), cv2.FONT_HERSHEY_SIMPLEX,
-                            0.9, qrcodeColor, 2)
+                self.__molduraQrCode(img, barcode, qrcodeColor, nome)
 
             img = cv2.flip(img, 1)
             cv2.imshow('Result', img)
             cv2.waitKey(1)
 
 
-camera = LerQrCode()
-camera.camera('workshopPython2022')
+camera = LerQrCode('workshopPython2022')
+camera.camera()
--- lerQrcode/presenca.py
+++ lerQrcode/presenca.py
@@ -2,17 +2,49 @@ from datetime import datetime
 import pandas as pd
 import os
 
+
 class Presenca():
+    def __init__(self, path: str):
+        self.__data = lambda: datetime.today().strftime('%Y-%m-%d')
+        self.__horario = lambda: datetime.today().strftime('%H-%M-%S')
+
+        self.__path = path
+
+        self.__fields = {'data': str, 'hora': str, 'cpf': str, 'nome': str}
+
     def __setFile(self, path: str) -> None:
         with open(path, 'w') as f:
             f.write('data,hora,cpf,nome\n')
 
-    def __getFile(self, path :str):
-        data = datetime.today().strftime('%Y-%m-%d')
-        path = f'{path}/presenca{data}.csv'
+    def getFile(self) -> pd.DataFrame:
+        path = f'{self.__path}/presenca.csv'
         
         if not os.path.exists(path):
             self.__setFile(path)
 
-        df = pd.read_csv(path)
+        df = pd.read_csv(path, usecols=self.__fields, dtype=self.__fields)
         df['cpf'] = df['cpf'].astype(str)
+
+        return df[df['data'] == self.__data()][['hora', 'nome', 'cpf']]
+
+    def checarInscricao(self) -> dict:
+        inscritos = {}
+
+        with open(f'{self.__path}/inscritos.txt') as f:
+            myDataList = f.read().splitlines()
+
+        for data in myDataList:
+            key, value = data.split('|')
+            inscritos[key] = value
+
+        return inscritos
+
+    def salvarDados(self, cpf: str, nome: str) -> pd.DataFrame:
+        with open(f'{self.__path}/presenca.csv', 'a') as p:
+            p.write(self.__data() + ',' + self.__horario() +
+                    ',' + cpf + ',' + nome + '\n')
+
+        df = pd.read_csv(f'{self.__path}/presenca.csv',
+                         usecols=self.__fields, dtype=self.__fields)
+
+        return df[df['data'] == self.__data()][['hora', 'nome', 'cpf']]
